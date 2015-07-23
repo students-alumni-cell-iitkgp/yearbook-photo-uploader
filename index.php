5df7e2d827d0bc9e6741e650119d87deae760e3f
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-23 18:58:35 +0530

    Improve frontend of index page
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>Upload photos for Yearbook 2016</title>
+    <style media="screen">
+      body {
+        text-align: center;
+      }
+    </style>
   </head>
   <body>
+    <h2>Enter your roll number to begin uploading photos:</h2>
     <form class="" method="post">
-      <input type="text" name="rollnumber" value="" placeholder="Roll Number">
+      <input type="text" name="rollnumber" value="" placeholder="Roll Number" required>
       <input type="submit" value="Submit Roll Number">
     </form>
   </body>
